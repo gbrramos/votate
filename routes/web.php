b0994efed363a38deaf1580c7f340e4c7ff55221
Gabriel Oliveira <gabriel.olv2002@gmail.com>
2021-07-05 20:25:50 -0300

    re-start with webpackmixjs

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('login.index');
+});
+
+Route::get('/login', function () {
+    return view('login.index');
 });
 
 // Route::get('v', function() {
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,8 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix .scripts('node_modules/jquery/dist/jquery.js', 'public/site/js/jquery.js')
+    .scripts('node_modules/bootstrap/dist/js/bootstrap.bundle.js', 'public/site/js/bootstrap.bundle.js')
+    
+
+    .sass('node_modules/bootstrap/scss/bootstrap.scss','public/site/bs/bootstrap.css');
