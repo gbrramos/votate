07bc818f9b9647d126ae13ab08a3cab97477d7ff
Gabriel Oliveira <gabriel.olv2002@gmail.com>
2021-07-06 12:43:33 -0300

    finish imports css e js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,15 @@
 const mix = require('laravel-mix');
 
 
-mix .scripts('node_modules/jquery/dist/jquery.js', 'public/site/js/jquery.js')
+mix 
+    /*jQuery JS*/ 
+    .scripts('node_modules/jquery/dist/jquery.js', 'public/site/js/jquery.js')
+
+    /*Boostrap JS*/
     .scripts('node_modules/bootstrap/dist/js/bootstrap.bundle.js', 'public/site/js/bootstrap.bundle.js')
     
+    /*Bootstrap Sass*/
+    .sass('node_modules/bootstrap/scss/bootstrap.scss','public/site/bs/bootstrap.css')
 
-    .sass('node_modules/bootstrap/scss/bootstrap.scss','public/site/bs/bootstrap.css');
+    /* Font Awesome CSS */
+    .styles('node_modules/@fortawesome/fontawesome-free/css/all.min.css','public/site/fa5/all.min.css');
